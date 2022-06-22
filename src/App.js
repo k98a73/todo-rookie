87af3a141cae3beb5bf951b3ee87a783bb9ef0a6
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-06-22 16:44:49 +0900

    追加機能の実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./reset.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const filterOptions = [
+    { value: "all", label: "すべて" },
+    { value: "notStarted", label: "未着手" },
+    { value: "inProgress", label: "作業中" },
+    { value: "done", label: "完了" },
+  ];
+
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo.trim(),
+        },
+      ]);
+    }
+    setTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header-title">TODOリスト</div>
+      <form className="input-area">
+        <input
+          name="todo"
+          type="text"
+          placeholder="TODOを入力"
+          value={todo}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleFormSubmit}>追加</button>
+      </form>
+      <div className="todo-area">
+        <ul>
+          {todos.map((todo, index) => {
+            return (
+              <li key={index}>
+                <div className="list-row">
+                  <p>
+                    id:{todo.id} {todo.text}
+                  </p>
+                  <select>
+                    {filterOptions.map(({ value, label }) => (
+                      <option key={label} value={value}>
+                        {label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
